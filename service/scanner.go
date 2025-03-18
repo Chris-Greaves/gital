7d5102712595b9c5c0756aa52cce68253564d7b4
Christopher Greaves <28542943+Chris-Greaves@users.noreply.github.com>
2025-03-18 04:30:00 +0000

    Start gathering git info on the repos found.

--- service/scanner.go
+++ service/scanner.go
@@ -6,6 +6,8 @@ import (
 	"log/slog"
 	"os"
 	"path/filepath"
+
+	"github.com/go-git/go-git/v5"
 )
 
 func ScanDirectories(ctx context.Context, directories []string) error {
@@ -44,9 +46,41 @@ func walkDirectory(ctx context.Context, directory string) error {
 			slog.Info("Git repository found", slog.String("path", filepath.Dir(path)))
 
 			// Gather information and store on a DB
+			err = gatherGitInfo(path)
+			if err != nil {
+				// Log error but continue
+				slog.Error("Error while gathering Git Repo Info", slog.String("path", filepath.Dir(path)), slog.Any("error", err))
+			}
 
 			return filepath.SkipDir // No need to walk through the .git folder
 		}
 		return nil
 	})
+}
+
+func gatherGitInfo(path string) error {
+	r, err := git.PlainOpen(path)
+	if err != nil {
+		return err
+	}
+
+	remotes, err := r.Remotes()
+	if err != nil {
+		return err
+	}
+
+	// List of remotes and their URLs
+	for _, rem := range remotes {
+		slog.Info("Found Remote on Repository", slog.String("path", filepath.Dir(path)), slog.String("remote_url", rem.Config().URLs[0]), slog.String("remote_url", rem.Config().Name))
+	}
+
+	// Get the HEAD reference (active branch)
+	headRef, err := r.Head()
+	if err != nil {
+		return err
+	}
+
+	// Get the current branch name
+	slog.Info("Current branch on Repository", slog.String("path", filepath.Dir(path)), slog.String("branch", headRef.Name().Short()))
+	return nil
 }
