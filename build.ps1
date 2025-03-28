2f69df5259f44c968d69b3f15c4657434e34fc7d
Christopher Greaves <28542943+Chris-Greaves@users.noreply.github.com>
2025-03-28 04:02:56 +0000

    Create initial project

--- build.ps1
+++ build.ps1
@@ -1,2 +1,8 @@
 # Build service
 go build -o ./bin/gital-service.exe ./service/
+
+# Build fat-client
+Set-Location .\fat-client 
+wails build -o gital.exe
+Set-Location ..\
+Copy-Item -Path "fat-client\build\bin\gital.exe" -Destination "bin\gital.exe"
