296c190700d88f4200c30dbc658beeca972b1e1a
Christopher Greaves <28542943+Chris-Greaves@users.noreply.github.com>
2025-04-08 01:26:11 +0100

    Move icons to their own folder

--- fat-client/frontend/src/Sidebar.svelte
+++ fat-client/frontend/src/Sidebar.svelte
@@ -1,4 +1,6 @@
 <script>
+    import AdjustableSliders from "./icons/AdjustableSliders.svelte";
+    import AngleRight from "./icons/AngleRight.svelte";
 </script>
 
 <aside
@@ -14,22 +16,9 @@
                     href="/"
                     class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
                 >
-                    <svg
-                        class="w-6 h-6 text-gray-800 dark:text-gray-400"
-                        aria-hidden="true"
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                    >
-                        <path
-                            stroke="currentColor"
-                            stroke-linecap="round"
-                            stroke-width="2"
-                            d="M6 4v10m0 0a2 2 0 1 0 0 4m0-4a2 2 0 1 1 0 4m0 0v2m6-16v2m0 0a2 2 0 1 0 0 4m0-4a2 2 0 1 1 0 4m0 0v10m6-16v10m0 0a2 2 0 1 0 0 4m0-4a2 2 0 1 1 0 4m0 0v2"
-                        />
-                    </svg>
+                    <AdjustableSliders
+                        style="text-gray-800 dark:text-gray-400"
+                    />
                     <span class="ms-3">General</span>
                 </a>
             </li>
@@ -67,23 +56,7 @@
                     href="/"
                     class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
                 >
-                    <svg
-                        class="w-6 h-6 text-gray-800 dark:text-gray-400"
-                        aria-hidden="true"
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                    >
-                        <path
-                            stroke="currentColor"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="2"
-                            d="m9 5 7 7-7 7"
-                        />
-                    </svg>
+                    <AngleRight style="text-gray-800 dark:text-gray-400" />
                     <span class="ms-3 mono-font text-xs">/path/to/repo</span>
                 </a>
             </li>
